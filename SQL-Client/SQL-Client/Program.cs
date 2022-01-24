2268c25e58c2241726189988af2dc45d84046652
s325909 <jaso_cyril@hotmail.com>
2022-01-24 13:00:58 +0100

    Feat: SqlConnectionStringBuilder

--- SQL-Client/SQL-Client/Program.cs
+++ SQL-Client/SQL-Client/Program.cs
@@ -2,7 +2,7 @@
 
 namespace SQL_Client
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
---
+++ SQL-Client/SQL-Client/SqlHelpers/SQLHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Client.SqlHelpers
+{
+    public class SQLHelper
+    {
+
+        public static string ConnectionString() 
+        {
+            SqlConnectionStringBuilder builder = new();
+
+            builder.DataSource = @"ND-5CG0255P6M\SQLEXPRESS";
+            builder.InitialCatalog = @"Chinook";
+            builder.IntegratedSecurity = true;
+            builder.TrustServerCertificate = true;
+
+
+            return builder.ConnectionString;
+        }
+
+     
+        
+       
+    }
+}
