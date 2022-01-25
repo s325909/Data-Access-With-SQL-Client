2c0cedc9e2d10c193311f20dbc24f1ddc31aeda4
s325909 <jaso_cyril@hotmail.com>
2022-01-25 20:57:32 +0100

    Chore: removed old data reads

--- SQL-Client/SQL-Client/Repositories/CustomerRepo.cs
+++ SQL-Client/SQL-Client/Repositories/CustomerRepo.cs
@@ -135,20 +135,6 @@ namespace SQL_Client.Repositories
                         cmd.Parameters.AddWithValue("@Offset", offset);
 
                         return DataReadHelper.GetCustomers(cmd);
-
-                        /**
-                         
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Customer tmpCustomer = GetReaderCustomer(reader);
-
-                                customersList.Add(tmpCustomer);
-                            }
-                        };
-
-                        **/
                     };
                 };
             }
@@ -246,32 +232,6 @@ namespace SQL_Client.Repositories
             {
                 Console.WriteLine(ex.ToString());
             }
-        }
-
-        private Customer GetReaderCustomer(SqlDataReader reader)
-        {
-            Customer customer = new();
-            customer.CustomerID = reader.GetInt32(0);
-
-            if (!reader.IsDBNull(1))
-                customer.FirstName = reader.GetString(1);
-
-            if (!reader.IsDBNull(2))
-                customer.LastName = reader.GetString(2);
-
-            if (!reader.IsDBNull(3))
-                customer.Country = reader.GetString(3);
-
-            if (!reader.IsDBNull(4))
-                customer.PostalCode = reader.GetString(4);
-
-            if (!reader.IsDBNull(5))
-                customer.PhoneNumber = reader.GetString(5);
-
-            if (!reader.IsDBNull(6))
-                customer.Email = reader.GetString(6);
-
-            return customer;
         }
     }
 }
