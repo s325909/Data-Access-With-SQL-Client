3a24bc3307b0f73d57ff6425434d81e4bac235e0
s325909 <jaso_cyril@hotmail.com>
2022-01-26 22:27:30 +0100

    Feat: Appendix A SQL scripts

---
+++ SQL-Client/SQL-Scripts/Appendix A/08_updateSuperhero.sql
@@ -0,0 +1,9 @@
+USE [SuperheroesDb]
+GO
+
+UPDATE [dbo].[Superhero]
+   SET [Name] = 'Dewald'
+ WHERE Id = 3;
+GO
+
+
---
+++ SQL-Client/SQL-Scripts/Appendix A/09_deleteAssistant.sql
@@ -0,0 +1,8 @@
+USE [SuperheroesDb]
+GO
+
+DELETE FROM [dbo].[Assistant]
+      WHERE Name = 'Robin';
+GO
+
+
